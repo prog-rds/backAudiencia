65b9cc8c7e133017c25e8a1990e643801afa6e04
Fabian Mendez <ofmendez@gmail.com>
2024-08-09 15:14:00 -0500

    chore: Update cors.js to allow requests from rds-la.com domain

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -4,7 +4,7 @@ export const corsMiddleware = () =>
 	cors({
 		origin: (o) => {
 			let result = 'http://localhost:5173';
-			if (o.startsWith('http://localhost') || o.startsWith('https://reddesignsystems.com') || o.endsWith('.reddesignsystems.com'))
+			if (o.startsWith('http://localhost') || o.startsWith('https://rds-la.com') || o.endsWith('.rds-la.com'))
 				result = o;
 			return result;
 		}
